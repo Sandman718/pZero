8f6b61d37b49bbd6a89729405f3143948171b869
Sandman718 <nyghost999@gmail.com>
2022-01-01 17:23:02 -0800

    commit

--- gameStore/Program.cs
+++ gameStore/Program.cs
@@ -3,84 +3,67 @@ using System.Collections.Generic;
 
 bool exit = false;
 //creating a dynamic list
-List<SignUpCustomer> toDoList = new List<SignUpCustomer>();
-do
-{
-    Console.WriteLine("Welcome to ToDo List");
-    Console.WriteLine("What would you like to do today?");
-    Console.WriteLine("1: Create a new todo item");
-    Console.WriteLine("2: View my list");
-    Console.WriteLine("3: Complete an item");
-    Console.WriteLine("x: Exit");
-    string input = Console.ReadLine();
-    // //instantiating an instance of our custom class
-    // ToDoItem item = new ToDoItem();
-    // //assigning values to the particular instance
-    // item.IsDone = false;
-    // item.Note = "Drink Coffee";
+List<SignUpCustomer> customerList = new List<SignUpCustomer>();
 
-    // //Here, we're using the class' methods
-    // Console.WriteLine(item.Print());
-    // item.CompleteItem();
-    // Console.WriteLine(item.Print());
+do{
 
-    switch(input)
-    {
+    Console.WriteLine("Welcome the Game Store Galore ");
+    Console.WriteLine("Make a selection");
+    Console.WriteLine("1 - Sign up");
+    Console.WriteLine("2 - Log in");
+    Console.WriteLine("3 - Inventory");
+    Console.WriteLine("4 - Cart");
+    Console.WriteLine("x - Exit");
+
+    //variable to store user input for our switch
+    string userInput = Console.ReadLine();
+  
+
+    switch (userInput) {
+
         case "1":
-            Console.WriteLine("Enter details for the new todo item");
-            Console.WriteLine("Item Note: ");
+            Console.WriteLine("What is your name: ");
+            Console.WriteLine(" ");
             //taking the user input and saving it as string var named newNote
-            string newNote = Console.ReadLine();
+            string name = Console.ReadLine();
 
-            ToDoItem newItem = new ToDoItem();
-            newItem.IsDone = false;
-            newItem.Note = newNote;
+            //new customer being created
+            SignUpCustomer newCustomer = new SignUpCustomer();
+            newCustomer.name = name;
 
-            toDoList.Add(newItem);
-            Console.WriteLine("You created the following item: ");
-            Console.WriteLine(newItem.Print());
+            customerList.Add(newCustomer);
+            Console.WriteLine($"Customer Name: {name} ");
+        
+           
+            Console.WriteLine(newCustomer.printCust());
         break;
+
         case "2":
-            //foreach loop loops through items in a collection
-            foreach(ToDoItem item in toDoList)
-            {
-                Console.WriteLine(item.Print());
-            }
+            Console.WriteLine("put in your credentials");
         break;
+
         case "3":
-            Console.WriteLine("Choose an item to complete");
-            //First, we're iterating through the list to display each listitem
-            //and its index (where they're located in the list)
-            //This index for item does not change
-            //so we can take advantage of that fact and 
-            //precisely locate the item
-            //Count is a property of generic collection List<T>
-            //where it returns the number of current items in the list
-            for(int i = 0; i < toDoList.Count; i++)
+            Console.WriteLine("Inventory");
+            //foreach loop loops through items in a collection
+            foreach(SignUpCustomer item in customerList)
             {
-                Console.WriteLine($"[{i}] {toDoList[i].Print()}");
-                // Console.WriteLine("[{0}] {1}", i, toDoList[i].Print());
+                Console.WriteLine(item.printCust());
             }
-
-            //Take in the user input and naively assume that users know what they are doing
-            //and parse it to integer so we can access the item by its index
-            int selection = int.Parse(Console.ReadLine());
-
-            //We found the item that the user wanted to complete
-            ToDoItem itemToComplete = toDoList[selection];
-
-            //we called the complete item method that marks IsDone property to true
-            itemToComplete.CompleteItem();
-
-            //Confirmation message
-            Console.WriteLine($"You completed {itemToComplete.Print()}");
+           
         break;
+
+        case "4":
+            Console.WriteLine("your cart");
+            break;
+
         case "x":
-            Console.WriteLine("Goodbye!");
+            Console.WriteLine("See you next time");
             exit = true;
         break;
+
         default:
-            Console.WriteLine("I'm not sure what that was");
+            Console.WriteLine("Try again ");
         break;
-    }
-} while(!exit);
+
+    }//end switch
+}while (!exit);//end while
